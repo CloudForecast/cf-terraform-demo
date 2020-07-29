f484e6e324cadb6d425bb75ae4c16ae22a7598bc
Karl Hughes <khughes.me@gmail.com>
2020-07-29 15:16:13 -0500

    Updating snapshot template

--- rds.tf
+++ rds.tf
@@ -28,15 +28,17 @@ resource "aws_security_group" "db" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mysql_db_1"
-  username             = var.db_username
-  password             = var.db_password
-  parameter_group_name = "default.mysql5.7"
+  allocated_storage         = 20
+  storage_type              = "gp2"
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  instance_class            = "db.t2.micro"
+  name                      = "mysql_db_1"
+  username                  = var.db_username
+  password                  = var.db_password
+  parameter_group_name      = "default.mysql5.7"
+  snapshot_identifier       = null
+  skip_final_snapshot       = true
 
   tags = {
     contact = "l-duke"
--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
-# CloudForecast Terraform Demonstration
-This repo contains a demonstration Terraform configuration.
+# CloudForecast - Terraform Demonstration
+This repo contains a sample Terraform configuration that sets up:
+
+- 2 ELB instances
+- 2 EC2 instances
+- 1 RDS database
+
+It is designed to demonstrate using tags to help system administrators
+keep their resources organized and maintained.
